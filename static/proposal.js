8575e127b2e1856b1bb941c5d36dfacf8eb0a165
eross04 <erinross@temple.edu>
2023-09-27 01:21:55 -0400

    Add css stylesheet to show it works and remove api key from NPS API call:

--- static/proposal.js
+++ static/proposal.js
@@ -1,4 +1,4 @@
-fetch('https://developer.nps.gov/api/v1/alerts?parkCode=dena&limit=10&api_key=0ilOFP8jTC2LMrwXFTullFqvHyVhBh9aHVW3OWEb'
+fetch('https://developer.nps.gov/api/v1/alerts?parkCode=dena&limit=10&api_key=PUT_KEY_HERE'
 	)
 	.then(response => response.json())
 	.then(response => console.log(response))
